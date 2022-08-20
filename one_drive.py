488a9cdcf6111f279e87d0ca0ea898705313c875
Aleksandar Stoiljkovic <aleksandar@dualsoft.net>
2022-08-20 18:02:22 +0200

    Odradjen upload na OneDrive - ukljucujuci i obnavljanje tokena i brisanje viska fajlova

--- one_drive.py
+++ one_drive.py
@@ -7,11 +7,12 @@ import pathlib
 import json
 import logging
 from timeit import default_timer as timer
+from dateutil import parser
 
 logger = logging.getLogger("backup_logger")
 
+
 class OneDrive:
-    
     def __init__(
         self,
         clientSecret,
@@ -19,9 +20,21 @@ class OneDrive:
         scopes,
         tokens=None,
         tokensFile='./tokens.json',
-        folderId=None,
-        tenantId=None
-        ):
+        tenantId="common"
+    ):
+        """This is the class to work with OneDrive. It consists of several parts:
+            generating and refreshing access token, uploading files to OneDrive,
+            removing old files from OneDrive
+
+        Args:
+            clientSecret (string): Client secret gained form Microsoft applicaiton
+            clientID (string): Client ID gained from Microsoft application
+            scopes (array): Array of Microsoft scopes ie. ['User.Read']
+            tokens (json, optional): JSON object that contains access and refres tokens. Defaults to None.
+            tokensFile (str, optional): File where JSON tokes will be stored. Defaults to './tokens.json'.
+            tenantId (string, optional): Micriostf Tenant ID where your application is beeing deployd. 
+                                         If you are using personal account backup system will use default "common".
+        """
         self.clientSecret = clientSecret
         self.clientID = clientID
         self.scopes = scopes
@@ -30,21 +43,21 @@ class OneDrive:
         self.__OAUTH2_BASE_URL = self.__LOGIN_BASE_URL + "/"+tenantId+"/oauth2/v2.0"
         self.__AUTHORIZE_BASE_URL = self.__OAUTH2_BASE_URL + "/authorize"
         self.__TOKENS_BASE_URL = self.__OAUTH2_BASE_URL + "/token"
-        self.__CHUNK_SIZE = 32768000  # velicina fragmenta trab da bude umnozak 320KB, trenutno je 31.25MB
+        # velicina fragmenta trab da bude umnozak 320KB, trenutno je 31.25MB
+        self.__CHUNK_SIZE = 32768000
         self.tokens = tokens
         self.tokensFile = tokensFile
-        self.folderId = folderId
-        
+
         self.__PerformLogin()
-        
+
     def __GetTokensFromFile(self):
-        if pathlib.Path(self.tokensFile).is_file() :
+        if pathlib.Path(self.tokensFile).is_file():
             try:
                 logger.debug("Opening file: "+self.tokensFile)
-                with open(self.tokensFile,'r') as f:
+                with open(self.tokensFile, 'r') as f:
                     content = f.read()
                     tokens = json.loads(content)
-                    logger.debug("Reading token from file: "+ str(tokens))
+                    logger.debug("Reading token from file: " + str(tokens))
                     f.close()
                     self.tokens = tokens
                     return True
@@ -55,23 +68,24 @@ class OneDrive:
             logger.debug("File: "+self.tokensFile+" doesn't exists !")
             self.tokens = None
             return False
-            
+
     def CreateAuthorizationUrl(self):
         authorization_url = (
-            self.__AUTHORIZE_BASE_URL + 
+            self.__AUTHORIZE_BASE_URL +
             '?client_id=' +
             self.clientID +
             '&response_type=code' +
-            '&scope='   
+            '&scope='
         )
-        for scope in self.scopes: 
+        for scope in self.scopes:
             if scope != "":
-                authorization_url += scope + ' '     
-        authorization_url=authorization_url[:-1]
-        authorization_url+='+offline_access+openid+profile'  
-        logger.debug("Created authorization URL: "+ authorization_url.replace(' ','%20'))
-        return authorization_url.replace(' ','%20')
-    
+                authorization_url += scope + ' '
+        authorization_url = authorization_url[:-1]
+        authorization_url += '+offline_access+openid+profile'
+        logger.debug("Created authorization URL: " +
+                     authorization_url.replace(' ', '%20'))
+        return authorization_url.replace(' ', '%20')
+
     def CheckTokens(self):
         if self.tokens != None:
             if self.tokens['access_token'] != None:
@@ -81,52 +95,51 @@ class OneDrive:
                 return True
         return False
 
-
-    def GetTokens(self,redirectionUrl):
+    def GetTokens(self, redirectionUrl):
         code = parse_qs(urlparse(redirectionUrl).query)['code'][0]
         headers = {
-            'Content-Type' : 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/x-www-form-urlencoded'
         }
         try:
             scope_post = ''
-            for scope in self.scopes: 
+            for scope in self.scopes:
                 if scope != "":
-                    scope_post += scope + ' '  
-            scope_post=scope_post[:-1]
+                    scope_post += scope + ' '
+            scope_post = scope_post[:-1]
             self.tokens = requests.post(url=self.__TOKENS_BASE_URL, data={
-                    'client_id' : self.clientID,
-                    'scope' : scope_post,
-                    'code' : code,
-                    'grant_type' : 'authorization_code',
-                    'client_secret' : self.clientSecret
-                }, headers=headers
+                'client_id': self.clientID,
+                'scope': scope_post,
+                'code': code,
+                'grant_type': 'authorization_code',
+                'client_secret': self.clientSecret
+            }, headers=headers
             ).json()
             self.__SaveTokensToFile()
         except Exception as e:
             logger.error("Error while getting tokens: "+str(e))
-        
+
     def __SaveTokensToFile(self):
         try:
-            with open(self.tokensFile,'w') as f:
+            with open(self.tokensFile, 'w') as f:
                 logger.debug("Opening file: "+self.tokensFile)
                 f.write(json.dumps(self.tokens))
                 logger.debug("Tokens are succesfully writed to a file")
                 f.close()
         except Exception as e:
             logger.error("Error while writing tokens to a file "+str(e))
-    
+
     def RenewTokens(self):
         headers = {
-            'Content-Type' : 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/x-www-form-urlencoded'
         }
 
         new_tokens = requests.post(url=self.__TOKENS_BASE_URL, data={
-                'client_id' : self.clientID,
-                'scope' : self.scopes,
-                'refresh_token' : self.tokens['refresh_token'],
-                'grant_type' : 'refresh_token',
-                'client_secret' : self.clientSecret
-            }, headers=headers
+            'client_id': self.clientID,
+            'scope': self.scopes,
+            'refresh_token': self.tokens['refresh_token'],
+            'grant_type': 'refresh_token',
+            'client_secret': self.clientSecret
+        }, headers=headers
         ).json()
 
         if self.tokens['access_token'] != new_tokens['access_token']:
@@ -137,32 +150,53 @@ class OneDrive:
             except Exception as e:
                 logger.warning("Keeping old refresh token!")
             self.__SaveTokensToFile()
-            
+        else:
+            logger.warning("New access token is same as old !")
+
     def __PerformLogin(self):
         if self.__GetTokensFromFile():
             logger.info("Successfully gathered tokens from file!")
         elif self.CheckTokens():
             logger.info("Tokens already exists!")
         else:
-            logger.info("Please go to this URL: "+self.CreateAuthorizationUrl())
+            logger.info("Please go to this URL: " +
+                        self.CreateAuthorizationUrl())
             logger.info("Insert URL where you been redirected:")
             redirectionUrl = input()
             self.GetTokens(redirectionUrl=redirectionUrl)
-            
-            
-    def UploadFile(self,oneDriveDir="",localDir=".",fileName="bck"):
+
+    def UploadFile(self, oneDriveDir="", localDir=".", fileName="bck"):
         if self.tokens == None:
             self.__PerformLogin()
         else:
             try:
+                uploadUrl = self.__GetUploadUrl(
+                    dir=oneDriveDir, fileName=fileName)
+                logger.debug("Upload URL: "+str(uploadUrl))
                 self.__UploadToOneDrive(
-                    url=self.__GetUploadUrl(dir=oneDriveDir,fileName=fileName),
+                    url=uploadUrl,
                     file=(localDir+'/'+fileName)
                 )
             except Exception as e:
-                logger.error("Error while performing upload to OneDrive: "+str(e))
-    
-    def __GetUploadUrl(self,dir,fileName):
+                logger.error(
+                    "Error while performing upload to OneDrive: "+str(e))
+
+    def __GetUploadUrl(self, dir, fileName):
+
+        response = self.__GetUploadUrlRequest(dir=dir, fileName=fileName)
+
+        if response.status_code >= 200 and response.status_code < 300:
+            logger.debug(response.json())
+            return response.json()['uploadUrl']
+        elif response.status_code == 401:
+            logger.warning("Access token is expired, renewing it! ")
+            self.RenewTokens()
+            return self.__GetUploadUrlRequest(dir, fileName).json()['uploadUrl']
+        else:
+            logger.error("Error while generating upload URL!")
+            return None
+
+    def __GetUploadUrlRequest(self, dir, fileName):
         request_body = {
             "item": {
                 "description": "Uploaded file from backup system.",
@@ -172,27 +206,18 @@ class OneDrive:
         headers = {
             'Authorization': 'Bearer '+self.tokens['access_token'],
         }
-        
-        getUploadSessionUrl = self.__GRAPH_API_URL + f'/me/drive/items/root:/'+ \
-        dir + '/'+ fileName+':/createUploadSession'
+
+        getUploadSessionUrl = self.__GRAPH_API_URL + f'/me/drive/items/root:/' + \
+            dir + '/' + fileName+':/createUploadSession'
+        logger.debug("URL for getting upload session: "+getUploadSessionUrl)
         response = requests.post(
             getUploadSessionUrl,
             headers=headers,
             json=request_body
         )
-        logger.debug(response)
-        if response.status_code >= 200 and response.status_code < 300:
-            return response.json()['uploadUrl']
-        elif response.status_code == 401:
-            logger.warning("Access token is expired, renewing it! ")
-            self.RenewTokens()
-            self.__GetUploadUrl(dir,fileName)
-        else:
-            logger.error("Error while generating upload URL!")
-            logger.error("URL for getting upload session: "+getUploadSessionUrl)
-            return None
-            
-    def __UploadToOneDrive(self,url,file):
+        return response
+
+    def __UploadToOneDrive(self, url, file):
         f = open(file)
         fileSize = os.path.getsize(file)
         uploadedBytes = 0
@@ -210,18 +235,22 @@ class OneDrive:
                 else:
                     sizeCurrRequest = self.__CHUNK_SIZE
                 logger.debug("Size of current fragment: "+str(sizeCurrRequest))
-                logger.debug("Current state : bytes "+str(uploadedBytes)+'-'+str(uploadedBytes+sizeCurrRequest-1)+'/'+str(fileSize))
+                logger.debug("Current state : bytes "+str(uploadedBytes) +
+                             '-'+str(uploadedBytes+sizeCurrRequest-1)+'/'+str(fileSize))
                 headers = {
                     'Content-Length': str(sizeCurrRequest),
-                    'Content-Range': 'bytes '+str(uploadedBytes)+'-'+str(uploadedBytes+sizeCurrRequest-1)+'/'+str(fileSize)
+                    'Content-Range': 'bytes '+str(uploadedBytes)+'-'
+                    + str(uploadedBytes+sizeCurrRequest-1)+'/'+str(fileSize)
                 }
                 data = f.read(sizeCurrRequest)
-                response = requests.put(url,data=data,headers=headers)
+                response = requests.put(url, data=data, headers=headers)
                 if response.status_code >= 300:
-                    logger.error("Error while uploading file: "+str(file)+" to OneDrive!")
-                    logger.error("Response code: "+response.status_code+"\nResponse text: "+response.text)
+                    logger.error("Error while uploading file: " +
+                                 str(file)+" to OneDrive!")
+                    logger.error("Response code: "+response.status_code +
+                                 "\nResponse text: "+response.text)
                     return
-                uploadedBytes+=sizeCurrRequest
+                uploadedBytes += sizeCurrRequest
         except Exception as e:
             logger.error(str(e))
         end = timer()
@@ -230,5 +259,115 @@ class OneDrive:
         response = requests.delete(url)
         logger.debug("Session successfuly terminated")
         logger.info("Time took for uploading: " +
-                str(timedelta(seconds=end - start)))
-            
+                    str(timedelta(seconds=end - start)))
+
+    def RemoveOldFiles(self, fileName, oneDriveDir, encrypt, noCopies=3):
+        try:
+            logger.debug("Fetching list of files")
+            list_of_files = self.__ListFilesForRemoval(
+                fileName=fileName,
+                oneDriveDir=oneDriveDir,
+                encrypt=encrypt,
+                noCopies=noCopies
+            )
+            keep_list = []
+            rem_list = []
+            for i in list_of_files:
+                if len(keep_list) == noCopies:
+                    if parser.parse(keep_list[len(keep_list)-1]["lastModifiedDateTime"]) < \
+                            parser.parse(i["lastModifiedDateTime"]):
+                        rem_list.append(keep_list[len(keep_list)-1])
+                        cnt = 0
+                        for j in keep_list:
+                            if parser.parse(i["lastModifiedDateTime"]) > \
+                                    parser.parse(j["lastModifiedDateTime"]):
+                                for k in range(noCopies-1, cnt, -1):
+                                    keep_list[k] = keep_list[k-1]
+                                keep_list[cnt] = i
+
+                                break
+                            cnt += 1
+                    else:
+                        rem_list.append(i)
+                elif len(keep_list) > 0:
+                    keep_list.append(i)
+                    keep_list.sort(key=lambda x: parser.parse(
+                        x["lastModifiedDateTime"]), reverse=True)
+                else:
+                    keep_list.append(i)
+
+            logger.info("Removing files:")
+            for i in rem_list:
+                self.__RemoveFile(i)
+            logger.debug("Keeping files:")
+            for i in keep_list:
+                logger.debug("File name: "+i['name'])
+        except Exception as e:
+            logger.error(str(e))
+
+    def __ListFilesForRemoval(self, fileName, oneDriveDir, encrypt, noCopies=3):
+        list_of_files = []
+        try:
+            headers = {
+                'Authorization': 'Bearer '+self.tokens['access_token'],
+            }
+
+            getListUrl = self.__GRAPH_API_URL + f'/me/drive/items/root:/' + \
+                oneDriveDir + ':/children'
+            logger.debug("URL for listing directory: "+getListUrl)
+            response = requests.get(
+                getListUrl,
+                headers=headers
+            )
+            if response.status_code >= 200 and response.status_code < 300:
+                for i in response.json()["value"]:
+                    if fileName in str(i["name"]):
+                        if encrypt:
+                            if str(i["name"]).endswith(".enc"):
+                                logger.debug("Find file: " + i["name"])
+                                list_of_files.append(i)
+                        else:
+                            if not str(i["name"]).endswith(".enc"):
+                                logger.debug("Find file: " + i["name"])
+                                list_of_files.append(i)
+
+            elif response.status_code == 401:
+                logger.warning("Access token is expired, renewing it! ")
+                self.RenewTokens()
+                list_of_files = self.__ListFilesForRemoval(
+                    fileName, oneDriveDir, encrypt, noCopies)
+            else:
+                logger.error("Listing directories failed with status code: " +
+                             str(response.status_code)
+                             + " and response message: " + response.json())
+
+        except Exception as e:
+            logger.error(str(e))
+
+        return list_of_files
+
+    def __RemoveFile(self, oneDriveElement):
+        try:
+            headers = {
+                'Authorization': 'Bearer '+self.tokens['access_token'],
+            }
+            deleteUrl = self.__GRAPH_API_URL + \
+                f'/me/drive/items/' + oneDriveElement['id']
+            logger.debug("URL for deleting file: "+deleteUrl)
+            response = requests.delete(
+                deleteUrl,
+                headers=headers
+            )
+            if response.status_code >= 200 and response.status_code < 300:
+                logger.info(
+                    "File: "+oneDriveElement['name']+" is successfully removed!")
+            elif response.status_code == 401:
+                logger.warning("Access token is expired, renewing it! ")
+                self.RenewTokens()
+                self.__RemoveFile(oneDriveElement)
+            else:
+                logger.error("Error wile deleting file: " +
+                             oneDriveElement['name']+" , with statsu code: " +
+                             str(response.status_code))
+        except Exception as e:
+            logger.error(str(e))
